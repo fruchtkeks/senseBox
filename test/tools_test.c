7ef02b9058b1b6c432eb2b0ee7f32389a07bdeaf
fruchtkeks <44132710+fruchtkeks@users.noreply.github.com>
2021-05-25 21:42:39 +0200

    Fixed wrong function name in tests/tools

--- test/tools_test.c
+++ test/tools_test.c
@@ -14,15 +14,15 @@ TEST_SETUP(tools) {}
 
 TEST_TEAR_DOWN(tools) {}
 
-TEST(tools, Append)
+TEST(tools, AppendString)
 {
 	char buffer[10];
 	memset(buffer, 0, 10);
 
-	TEST_ASSERT(tools_append(buffer, "abcde", 10) == 0);
+	TEST_ASSERT(tools_appendString(buffer, "abcde", 10) == 0);
 	TEST_ASSERT(strlen(buffer) == 5);
-	TEST_ASSERT(tools_append(buffer, "fghi", 10) == 0);
+	TEST_ASSERT(tools_appendString(buffer, "fghi", 10) == 0);
 	TEST_ASSERT(strlen(buffer) == 9);
-	TEST_ASSERT(tools_append(buffer, "j", 10) == 1);
+	TEST_ASSERT(tools_appendString(buffer, "j", 10) == 1);
 	TEST_ASSERT(strlen(buffer) == 9);
 }
