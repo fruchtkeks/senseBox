76c3391b4418968e273e24897fe8ca7b9d0c6fd6
fruchtkeks <44132710+fruchtkeks@users.noreply.github.com>
2021-06-20 17:35:22 +0200

    Added calculation of measurement delay to unit tests

--- test/sensor_test.c
+++ test/sensor_test.c
@@ -14,15 +14,17 @@ TEST_TEAR_DOWN(sensor) {}
 TEST(sensor, Init)
 {
 	struct bme280_dev device;
+	uint32_t delay_in_us = 0;
 
-	TEST_ASSERT(sensor_init(&device) == 1);
+	TEST_ASSERT(sensor_init(&device, &delay_in_us) == 1);
 }
 
 TEST(sensor, Read)
 {
 	struct bme280_dev device;
+	uint32_t delay_in_us = 0;
 	struct measurements measurements;
 
-	TEST_ASSERT(sensor_init(&device) == 1);
-	TEST_ASSERT(sensor_read(&device, &measurements) == 1);
+	TEST_ASSERT(sensor_init(&device, &delay_in_us) == 1);
+	TEST_ASSERT(sensor_read(&device, &measurements, delay_in_us) == 1);
 }
